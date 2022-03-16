d0bb9e3d19a972d54bb60c2b0444202e208b1b13
Albende <albendemu@gmail.com>
2022-03-16 11:16:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import Header from "./Header.js";
 import MySongs from "./MySongs.js";
 import Search from "./Search.jsx";
 function App() {
   return (
     <div className="App">
-      <Header />
       <div className="body">
         <MySongs />
         <Search />
